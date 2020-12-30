52c1e30376cc54ca63dfee8c98e72f969b45c697
CallMeGray-dot <76544108+CallMeGray-dot@users.noreply.github.com>
2020-12-30 14:04:36 +0800

    Update welcome.js

--- util/welcome.js
+++ util/welcome.js
@@ -7,4 +7,10 @@ client.on('guildMemberAdd', (member) => {
      if(member.guild.id != ChannelID) return;
     channel.send(`Welcome to Polar support server, <@${member.id}> Please read rules in <#792313562067894290>`)
     }) //You Can Any Cutom Message Here Above^
+client.on('guildMemberRemove', (member) => {
+    
+    const channel = client.channels.cache.get(ChannelID)
+     if(member.guild.id != ChannelID) return;
+    channel.send(`<@${member.id}> just left i hope he comes back`)
+    }) //You Can Any Cutom Message Here Above^
 }
