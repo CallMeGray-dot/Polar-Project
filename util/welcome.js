4109fe752c0d96ce4c922ade096090706307fa6d
CallMeGray-dot <76544108+CallMeGray-dot@users.noreply.github.com>
2020-12-30 14:27:12 +0800

    Update welcome.js

--- util/welcome.js
+++ util/welcome.js
@@ -1,16 +1,14 @@
 module.exports = (client) => {  
 const ChannelID ='793678213884674048' //Channel ID Where you want to send Welcome Message
 
-client.on('guildMemberAdd', (member) => {
-    
+client.on('guildMemberAdd', member => {
     const channel = client.channels.cache.get(ChannelID)
-     if(member.guild.id != ChannelID) return;
+     if(member.guild.id != '792313562060029972') return;
     channel.send(`Welcome to Polar support server, <@${member.id}> Please read rules in <#792313562067894290>`)
-    }) //You Can Any Cutom Message Here Above^
+    })
 client.on('guildMemberRemove', (member) => {
-    
     const channel = client.channels.cache.get(ChannelID)
-     if(member.guild.id != ChannelID) return;
-    channel.send(`<@${member.id}> just left i hope he comes back`)
-    }) //You Can Any Cutom Message Here Above^
+     if(member.guild.id != '792313562060029972') return;
+    channel.send(`<@${member.id}> just left, i hope he comes back`) 
+    })
 }
