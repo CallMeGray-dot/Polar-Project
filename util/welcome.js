452d84f5d04be53bf33d26ff01deacbe61c244d3
CallMeGray-dot <76544108+CallMeGray-dot@users.noreply.github.com>
2021-01-07 13:12:21 +0800

    Update welcome.js

--- util/welcome.js
+++ util/welcome.js
@@ -1,14 +1,27 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
 module.exports = (client) => {  
 const ChannelID ='793678213884674048' //Channel ID Where you want to send Welcome Message
 
-client.on('guildMemberAdd', member => {
+client.on('guildMemberAdd', (member, guild) => {
     const channel = client.channels.cache.get(ChannelID)
-     if(member.guild.id != '792313562060029972') return;
-    channel.send(`Welcome to Polar support server, <@${member.id}> Please read rules in <#792313562067894290>`)
+    let avatar = member.user.displayAvatarURL({ dynamic: true })
+    const embed = new Discord.MessageEmbed()
+    .setTitle('We have a new member!')
+    .setThumbnail(avatar)
+    .setDescription(`Welcome <@${member.id}>! to the server! Read <#792313562067894290> to get started! And learn about Polar.`)
+    if(member.guild.id != '792313562060029972') return;
+    channel.send(embed)
     })
-client.on('guildMemberRemove', (member) => {
-    const channel = client.channels.cache.get(ChannelID)
+client.on('guildMemberRemove', (member, guild) => {
+    const channel = client.channels.cache.get(ChannelID) 
+    let avatar1 = member.user.displayAvatarURL({ dynamic: true })
+    const embed = new Discord.MessageEmbed()
+    .setTitle('We lost a member')
+    .setThumbnail(avatar1)
+    .setDescription(`<@${member.id}> just left the server! i hope he comes back`)
      if(member.guild.id != '792313562060029972') return;
-    channel.send(`<@${member.id}> just left, i hope he comes back`) 
+    channel.send(embed) 
     })
 }
