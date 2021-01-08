4049f8fae93c19efcf13dfc8e543c0021d80187d
CallMeGray-dot <76544108+CallMeGray-dot@users.noreply.github.com>
2021-01-08 15:20:02 +0800

    Update minecraft-cmd.js

--- commands/games/minecraft-cmd.js
+++ commands/games/minecraft-cmd.js
@@ -2,18 +2,16 @@ const Discord = require('discord.js');
 const util = require('minecraft-server-util');
 
 module.exports = {
-    commands: ['serverstatus', 'minecrafserverstatus', 'mss'],
-    description: 'Shows info about the minecraft server given',
-    minArgs: 0,
-    maxArgs: null,
-    usage: '<ip>',
-    callback: async (client, message, args) => {
-const ip = message.content.split(' ').args.slice(0).join(' ');
-if(!ip) return message.reply('You must put an ip of a minecraft server!')
+    commands: ['mss', 'minecraftserverstatus'],
+    description: 'Shows minecraft server status',
+    usage: 'ip',
+    callback: async (message, args) => {
+const ip = message.content.split(' ').slice(1).join(' ');
+if(!ip) return message.reply('¡Debes colocar una ip de un servidor de minecraft!')
         util.status(ip) // port is default 25565
     .then((response) => {
         const embed = new Discord.MessageEmbed()
-        .setDescription(`Ip of the server: ${response.host}\nOnline Players: ${response.onlinePlayers}\nMax players: ${response.maxPlayers}\nVersion: ${response.version}`)
+        .setDescription(`Ip of server: ${response.host}\nOnline players: ${response.onlinePlayers}\nMax players: ${response.maxPlayers}\nVersion: ${response.version}`)
         .setColor('RANDOM')
         message.channel.send(embed);
     });    
