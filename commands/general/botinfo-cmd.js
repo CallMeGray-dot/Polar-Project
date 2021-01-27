0be463c9fdaae72a118c31633a2c150ef1baf494
CallMeGray-dot <76544108+CallMeGray-dot@users.noreply.github.com>
2021-01-27 08:25:03 +0800

    Update botinfo-cmd.js

--- commands/general/botinfo-cmd.js
+++ commands/general/botinfo-cmd.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js')
 
 module.exports = {  
-   commands: ['botinfo', 'bot-info'],
+   commands: ['botinfo', 'bot-info', 'stats'],
    description: 'Shows bot info',
    callback: (message, args) => {
+   const ramdt = (`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB Ram`);
    const info = new Discord.MessageEmbed()
    .setTitle('Here is my bot info!')
    .setDescription('Here is all the info!')
@@ -13,7 +14,8 @@ module.exports = {
    { name: `Owner`, value: 'Jigr#0398, CallMeGray#0621', inline: true },
    { name: `Uptime`, value: '98.97%', inline: true },
    { name: `Commands`, value: '129 Commands in total', inline: true },
-   { name: `Host`, value: 'Hosted Using: QuackHost.uk', inline: true }
+   { name: `Host`, value: 'Hosted Using: QuackHost.uk', inline: true },
+   { name: `Ram Usage`, value: ramdt, inline: true}
 )
 message.channel.send(info)
  }
